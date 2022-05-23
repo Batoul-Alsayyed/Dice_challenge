07801f58785c26bb41cfad487ea041afb162e7bd
Batoul <102414385+Batoul447@users.noreply.github.com>
2022-05-23 10:32:50 +0300

    loading the 2 image classes

--- styles.js
+++ styles.js
@@ -2,4 +2,7 @@ document.addEventListener("DOMContentLoaded",function(){
     var num1 = Math.floor((Math.random()*6)+1) // picking random number from 1 to 6 
     var num2 = Math.floor((Math.random()*6)+1)
    
+    var image1 = document.getElementsByClassName("img1")[0]
+    var image2 = document.getElementsByClassName("img2")[0]
+
 })
