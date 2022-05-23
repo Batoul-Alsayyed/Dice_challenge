17b862e080abc2dfc797b43af9d12dbaf3c32c01
Batoul <102414385+Batoul447@users.noreply.github.com>
2022-05-23 10:35:21 +0300

    Adding if conditions

--- styles.js
+++ styles.js
@@ -10,4 +10,66 @@ document.addEventListener("DOMContentLoaded",function(){
     src1 = "images/"
     src2 = "images/"
 
+    
+    if (num1==1){
+        //load image1
+        src1+="dice1.png"
+    }
+    else if (num1==2){
+        //load image2
+        src1+="dice2.png"
+
+    }
+    else if (num1==3){
+        //load image 3
+        src1+="dice3.png"
+
+    }
+    else if (num1==4){
+        //load image 4
+        src1+="dice4.png"
+
+    }
+    else if (num1==5){
+        //load image 5
+        src1+="dice5.png"
+
+    }
+    else if (num1==6){
+        //load image 6
+        src1+="dice6.png"
+
+    }
+
+    if (num2==1){
+        //load image1
+        src1+="dice1.png"
+
+    }
+    else if (num2==2){
+        //load image2
+        src1+="dice2.png"
+
+    }
+    else if (num2==3){
+        //load image 3
+        src1+="dice3.png"
+
+    }
+    else if (num2==4){
+        //load image 4
+        src1+="dice4.png"
+
+    }
+    else if (num2==5){
+        //load image 5
+        src1+="dice5.png"
+
+    }
+    else if (num2==6){
+        //load image 6
+        src1+="dice6.png"
+
+    }
+ 
 })
