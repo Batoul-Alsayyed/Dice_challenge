14e5fbc0457ce857d33b65567c69ce51c3c055eb
Batoul <102414385+Batoul447@users.noreply.github.com>
2022-05-23 10:32:12 +0300

    Generate 2 random numbers

---
+++ styles.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded",function(){
+    var num1 = Math.floor((Math.random()*6)+1) // picking random number from 1 to 6 
+    var num2 = Math.floor((Math.random()*6)+1)
+   
+})
